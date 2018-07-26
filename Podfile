783082d016c90791cfaecf66a1cba200c015299b
Dante P <dantejpuglisi@gmail.com>
2018-07-26 09:43:13 -0300

    Added Cocoapods and Alamofire

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '11.0'
+
+target 'Sherpany Interview Project' do
+  use_frameworks!
+
+  pod 'Alamofire', '~> 4.7'
+
+end
---
+++ Pods/Target Support Files/Pods-Sherpany Interview Project/Pods-Sherpany Interview Project-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sherpany_Interview_Project : NSObject
+@end
+@implementation PodsDummy_Pods_Sherpany_Interview_Project
+@end
